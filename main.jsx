6752dc2e1b07953d36d2d895f67a290dfdf4dcdb
avikgra-stack <avikgra@gmail.com>
2026-01-02 02:50:07 +0500

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,1 +1,9 @@
-
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="UTF-8" />
+    <title>My AI Landing</title>
+  </head>
+  <body>
+    <div id="root"></div> <script type="module" src="/src/main.jsx"></script> </body>
+</html>
